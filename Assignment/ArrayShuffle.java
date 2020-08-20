3ffa72e362aabaf325d21a29ebd9cd2000fc7150
illusionrs <rajeevshukla255@gmail.com>
2020-08-20 22:42:33 +0530

    Array shuffle leetcode 1470

---
+++ Assignment/ArrayShuffle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class ArrayShuffle{
+
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2 * n];
+       for (int i = 0, j = n, idx = 0; idx < res.length; i++, j++) {
+           res[idx++] = nums[i];
+           res[idx++] = nums[j];
+       } 
+       return res;
+       
+              
+   }
+}
